dbd15f11d616bf7252b9398a06fa35576a025d34
CaioHaack <caiohaackmello@gmail.com>
2022-06-02 09:56:47 -0300

    Estudos PHP

---
+++ estudos-php/FundamentosBasicosPHP.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Fundamentos básicos</title>
+</head>
+<body>
+<?php
+//Aqui vai todo código php
+/*aqui vai mais um*/
+
+echo "Meu nome é Caio. <br>";
+print "Prazer em te conhecer, humano.";
+//variaveis 
+$nome = "Caio";
+$idade = 14;
+$preco = 8.84;
+
+echo "Meu nome é $nome, tenho $idade anos e estou vendendo laranjas por $preco";
+?>
+<br>
+
+</body>
+</html>
