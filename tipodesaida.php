268775b44c9c69d6c1738ed3dfcadded41808932
Caio M. Haack <102704330+CaioMelloh@users.noreply.github.com>
2022-06-10 23:41:56 -0300

    Delete tipodesaida.php

--- tipodesaida.php
+++
@@ -1,7 +0,0 @@
-<?php
-/* o echo equivale ao system.out.print do Java*/ 
-$nome = "Caio";
-$sobrenome = "Mello";
-echo "O nome do usuário é $nome $sobrenome";
-//se utilizar uma tag html que utiliza aspas duplas você pode trocar o externo por duplas e o interno por simples;
-print $nome; /*-> tipo de comando de saída, menos usado que o echo
