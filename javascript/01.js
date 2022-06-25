0bfdddfff44313c341b62ef95d61f4dc9e54672f
CaioHaack <caiohaackmello@gmail.com>
2022-06-25 20:59:44 -0300

    aulas de js

---
+++ javascript/01.js
@@ -0,0 +1,17 @@
+let nome = 'Caio Mello';
+let altura = 1.84;
+
+console.log('Seja bem-vindo,', nome, "!\n", "Sua altura é ", altura, 'cm');
+// linguagem case sensitive 
+
+
+// objeto pessoa
+let pessoa = {
+    nome : "Caio Mello",
+    idade : 25,
+    cargo : 'oficial aposentado da marinha',
+    nacionalidade: 'portuga',
+    idiomas: 'português, inglês, alemão e frânces'
+};
+
+console.log(pessoa);
