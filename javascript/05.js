d4b71ede771fc00780cc952de509e6164187ca1c
CaioHaack <caiohaackmello@gmail.com>
2022-06-27 10:54:13 -0300

    Estudos html e css

--- javascript/05.js
+++ javascript/05.js
@@ -12,3 +12,6 @@ console.log(num**2);
 num = 24;
 console.log(++num); /*executa a operação antes de printar*/
 console.log(num); /*impressão já incrementada*/
+console.log(1===1);
+/*compara o  valor e o tipo*/
+console.log(1==='1');
