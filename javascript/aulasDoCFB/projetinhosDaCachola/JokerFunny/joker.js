e4291ad739d81b01f23935d31fb45ced52835d44
CaioHaack <caiohaackmello@gmail.com>
2022-07-22 22:28:19 -0300

    t

---
+++ javascript/aulasDoCFB/projetinhosDaCachola/JokerFunny/joker.js
@@ -0,0 +1,7 @@
+function abrir(){
+	var abrir = document.getElementById("abra").setAttribute("open", "");
+
+}
+function fechar(){
+	var fechar = document.getElementById("abra").removeAttribute("open");
+}
